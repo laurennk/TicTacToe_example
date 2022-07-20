cdf385fa5d338d153f5867c71e0093756b8c9135
laurennk <laurennk13@gmail.com>
2022-07-20 11:45:55 -0700

    Finished zoom demo tic tac toe

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,95 +2,78 @@ import numpy as np
 import random
 
 
-def print_board(board_array):
-    print("Printing board")
+def play_game():
+    print("Let's play TicTacToe!")
 
-    extra_line = "     |     |"
-    new_line = "\n-----|-----|-----\n"
-    
-    print(extra_line)
-    print(" ", board_array[0][0], " | ", board_array[0][1], " | ", board_array[0][2])
-    print(new_line)
-    print(" ", board_array[1][0], " | ", board_array[1][1], " | ", board_array[1][2])
-    print(new_line)
-    print(" ", board_array[2][0], " | ", board_array[2][1], " | ", board_array[2][2])
-    print(extra_line)
+    board_options = np.array([['0', '1', '2'],
+                              ['3', '4', '5'],
+                              ['6', '7', '8']])
 
-    return
+    print_board(board_options)
+
+    board = init_board()
+
+    win = False
+
+    character = determine_start()
+
+    while not win:
+
+        take_turn(character, board)
+
+        print_board(board)
+
+        if check_win(character, board):
+            again = input("Would you like to play again? [Y/N]")
+
+            if again == 'Y' or again == 'y':
+                print("Resetting board")
+                board = init_board()
+                character = determine_start()
+            else:
+                print("Thanks for playing!")
+                exit(0)
+        else:
+            character = switch_character(character)
 
 
-def init_board():
-    print("Initializing a new board")
-    board = np.array([[" ", " ", " "],
-                      [" ", " ", " "],
-                      [" ", " ", " "]])
-
-    return board
+def switch_character(current_character):
+    if current_character == "X":
+        return "O"
+    else:
+        return "X"
 
 
 def take_turn(current_character, board):
-    print("Starting turn")
 
-    # placeholder
-    move = input(current_character + " what is your move?")
+    move = input(current_character + ", where would you like to go? ")
 
-    # check if the move is valid
     while not valid_move(int(move), board):
-        move = input("That is not a valid move. Try again:")
+        move = input(current_character + ", where would you like to go? ")
 
-    # update board
-    update_board(current_character, int(move), board)
+    new_board = update_board(current_character, board, int(move))
 
-
-def update_board(character, move, board):
-    print("Updating the board")
-    
-    board[move//3][move%3] = character
-
-
-def determine_start():
-    print("Determining the start of the game")
-
-    possible_start = ['X', 'O']
-    choice = random.choice(possible_start)
-
-    return choice
-
-
-def valid_move(move, board):
-    print("Checking if the move is valid")
-
-    if move > 8:
-        return False
-        
-    if board[move//3][move%3] == ' ':
-        return True
-    elif board[move//3][move%3] == 'X':
-        print("There is an X there!")
-        return False
-    else:
-        print("There is an O there!")
-        return False
+    return new_board
 
 
 def check_win(character, board):
-    print("Checking if the game has been won")
-
-    win = check_rows(board)
-    print("Rows", win)
-    if win == False:
-        win = check_columns(board)
-        print("Columns", win)
-    if win == False:
-        win = check_diagonals(board)
-        print("Diag", win)
-
-    print(win)
-    return win
+    if check_rows(board):
+        print(character, " has won!")
+        return True
+    elif check_columns(board):
+        print(character, " has won!")
+        return True
+    elif check_diagonals(board):
+        print(character, " has won!")
+        return True
+    elif " " not in board:
+        print("It's a tie! No one has won!")
+        return True
+    else:
+        return False
 
 
 def check_rows(board):
-    print("Checking rows")
 
     for i in range(3):
         if board[i][0] == " ":
@@ -101,21 +84,20 @@ def check_rows(board):
     return False
 
 
-
 def check_columns(board):
-    print("Checking columns")
     for i in range(3):
         if board[0][i] == " ":
             continue
         elif (board[0][i] == board[1][i]) and (board[1][i] == board[2][i]):
             return True
+
     return False
 
+
 def check_diagonals(board):
-    print("Checking diagonals")
     if board[1][1] == " ":
         return False
-    elif (board[0][0] == board[1][1]) and (board[1][1] == board[2][1]):
+    elif (board[0][0] == board[1][1]) and (board[1][1] == board[2][2]):
         return True
     elif (board[0][2] == board[1][1]) and (board[1][1] == board[2][0]):
         return True
@@ -123,74 +105,53 @@ def check_diagonals(board):
         return False
 
 
+def update_board(character, board, move):
+    board[move//3][move%3] = character
+    return board
 
-def switch_player(character):
-    print("Switching player")
-    if character == 'X':
-        character = 'O'
+
+def valid_move(move, current_board):
+    if move > 8:
+        print("That is an invalid move!")
+        return False
+    elif current_board[move//3][move%3] == " ":
+        return True
+    elif current_board[move//3][move%3] == "X":
+        print("There is already an X there!")
+        return False
+    elif current_board[move//3][move%3] == "O":
+        print("There is already an O there!")
+        return False
     else:
-        character = 'X'
+        print("That is an invalid move!")
+        return False
 
-    return character
 
+def determine_start():
+    possible_starts = ['X', 'O']
+    choice = random.choice(possible_starts)
+    print(choice, "will go first!")
 
-def play_game():
-    print("Entering play game")
+    return choice
 
-    # declare the board options
-    board_options = np.array([['0', '1', '2'],
-                              ['3', '4', '5'],
-                              ['6', '7', '8']])
+def init_board():
+    new_board = np.array([[' ', ' ', ' '],
+                          [' ', ' ', ' '],
+                          [' ', ' ', ' ']])
 
-    # print the board options
-    print_board(board_options)
+    return new_board
 
-    # initialize the board
-    board = init_board()
+def print_board(board):
 
-    # keep track of if the game has been won
-    win = False
+    new_line = "-----------------"
 
-    # determine how the game will be started
-    player = determine_start()
+    print("\n ", board[0][0], " | ", board[0][1], " | ", board[0][2])
+    print(new_line)
+    print(" ", board[1][0], " | ", board[1][1], " | ", board[1][2])
+    print(new_line)
+    print(" ", board[2][0], " | ", board[2][1], " | ", board[2][2], "\n")
 
-    print("Player " + player + " will start the game")
-
-    # while the game has not been won stay inside of a loop
-    while not win:
-        # take turns
-        take_turn(player, board)
-
-        # print the board after each turn
-        print_board(board)
-
-        # check if the  game has been won after each turn
-        if check_win(player, board):
-            # if the game has been won, ask if they would like to play again
-            again = input("Would you like to play again? [Y/N]")
-
-            # if they would like to play again...
-            if (again == 'Y') or (again == 'y'):
-                # reset the board
-                print("Resetting board")
-                board = init_board()
-
-                # determine how the game will be started
-                player = determine_start()
-
-            # if they would not like to play again...
-            elif (again == 'N') or (again == 'n'):
-                # exit the program
-                print("Thanks for playing!")
-                exit(0)
-
-        # swap which players turn it is at the end of each turn
-        player = switch_player(player)
-        
-        
 
 
 if __name__ == "__main__":
     play_game()
-
-
